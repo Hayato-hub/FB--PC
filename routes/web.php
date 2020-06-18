814e82e662e3dc716ade306756dc1eb52290274b
EC2 Default User <ec2-user@ip-172-31-46-128.us-east-2.compute.internal>
2020-06-18 05:12:35 +0000

    mm

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,22 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('job', function () {
+    return view('fb.check'); //仮LP
+    // return view('fb.job'); //獲得用LP
+});
+
+//イククル-DL
+Route::get('dl', function () {
+    return view('fb.dl'); //自動遷移LP。ココに案件タグを設置
+});
+
+//popo-DL
+Route::get('install', function () {
+    return view('fb.install'); //自動遷移LP。ココに案件タグを設置
+});
+
+// LP確認用
+Route::get('cccheck', function () {
+    return view('fb.job'); //獲得LP
 });
